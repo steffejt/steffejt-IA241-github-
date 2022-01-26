443163316fc00929351406cd56056618b3280a68
EC2 Default User <ec2-user@ip-172-31-9-243.ec2.internal>
2022-01-26 20:04:39 +0000

    lecture 2

---
+++ lec2.py
@@ -0,0 +1,22 @@
+'''
+lec 2
+this is a regional
+'''
+import math
+# print('hello world') # this is a single line comment
+
+# print(type(123))
+
+# print(type(123.))
+
+# print(type('123'))
+
+# print("Tom's cat")
+
+# print('Hello ' + 'world')
+
+# print(math.sqrt(4))
+
+my_str = 'hello world'
+
+print(my_str.upper())
